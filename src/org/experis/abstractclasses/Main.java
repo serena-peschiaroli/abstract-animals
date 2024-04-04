a93a447798b235648a6ac16b89a9c9a82f9db37b
serena.peschiaroli <serena.peschiaroli@gmail.com>
2024-04-04 15:01:48 +0200

    Fix: formatting in print statements

--- src/org/experis/abstractclasses/Main.java
+++ src/org/experis/abstractclasses/Main.java
@@ -5,10 +5,14 @@ public class Main {
         Animal[] animals = {new Dog("Milo"), new Sparrow("Pip"), new Eagle("Orion"), new Dolphin("Echo")};
 
         for (Animal animal : animals){
-            System.out.println(animal + animal.getName());
+            //separatore per maggiore chiarezza
+            System.out.println("------------------------------");
+            // s% placeholder per animal.getName e animal.getClass.getSimpleName--> ritorna il nome della classe
+            System.out.printf("%s the %s : \n", animal.getName(), animal.getClass().getSimpleName());
             animal.makeNoise();
             animal.eat();
             animal.sleep();
+            System.out.println("------------------------------\n");
         }
     }
 }
